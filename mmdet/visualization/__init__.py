7b0937d30c1d5f9590bdeb3802f3de4c4d4be0a3
hsj576 <sjhu21@m.fudan.edu.cn>
2023-08-21 11:34:05 +0800

    add imshow_det_bboxes
    Signed-off-by: hsj576 <sjhu21@m.fudan.edu.cn>

--- mmdet/visualization/__init__.py
+++ mmdet/visualization/__init__.py
@@ -3,6 +3,7 @@ from .local_visualizer import DetLocalVisualizer, TrackLocalVisualizer
 from .palette import get_palette, jitter_color, palette_val
 
 __all__ = [
+    'imshow_det_bboxes',
     'palette_val', 'get_palette', 'DetLocalVisualizer', 'jitter_color',
     'TrackLocalVisualizer'
 ]
